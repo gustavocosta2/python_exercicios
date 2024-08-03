859a0bc2ef3b82df04e60e270899415cc9bdd534
Gustavo Costa <gustavocosta.ds09@gmail.com>
2024-08-03 19:08:02 -0300

    Add files via upload

---
+++ exercicios-variaveis-e-funcoes-basicas/002-exercicio-input.py
@@ -0,0 +1,5 @@
+# Exercicio 002
+# Faça um programa que peça ao usuário para digitar um número e, em seguida, imprima na tela a mensagem "O número digitado foi <numero>"
+
+numero = input("Digite um número: ")
+print(f"O número digitado foi {numero}")
---
+++ exercicios-variaveis-e-funcoes-basicas/003-exercicio-input.py
@@ -0,0 +1,5 @@
+# Exercicio 003
+# Faça um programa que peça ao usuário para digitar seu nome e, em seguida, imprima na tela a mensagem "Olá, <nome>!"
+
+nome = input("Qual é o seu nome?")
+print(f"Olá, {nome}!")
