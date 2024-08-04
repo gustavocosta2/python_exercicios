9f09e9f2f2cef272b268587c95b94844140516e6
Gustavo Costa <gustavocosta.ds09@gmail.com>
2024-08-04 15:19:37 -0300

    Create 004-exercicio.py

---
+++ exercicios_tipos_de_dados/004-exercicio.py
@@ -0,0 +1,9 @@
+# Exercicio 004
+# Faça um programa que realize a soma e a subtração de duas variáveis, sendo uma do tipo inteiro e outra do tipo decimal, exibindo o valor da soma
+# e da subtração no seguinte formato: "O valor da soma é: <soma>" e "O valor da subtração é: <subtracao>"
+
+v1 = 10
+v2 = 9.5
+
+print(f"O valor da soma é: {v1+v2}")
+print(f"O valor da subtração é: {v1-v2}")
